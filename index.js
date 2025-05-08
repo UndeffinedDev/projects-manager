eafa7dca1e87e893b0310f0a2f3eb4b534519586
Sergio Caguana <undeffineddev419@gmail.com>
2025-05-07 21:48:08 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const fs = require('fs')
+const app = express()
+const port = 3000
+
+let rawData = fs.readFileSync('info.json')
+let projectData = JSON.parse(rawData)
+let projects = projectData.projects
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/projects', (req, res) => {
+  res.json(projects)
+})
+
+app.get('/projects/:project_name', (req, res) => {
+  const projectName = req.params.project_name
+  const project = projects.find(p => p.id.toLowerCase() === projectName.toLowerCase())
+
+  if (project) {
+    res.json(project)
+  } else {
+    res.status(404).send('Proyecto no encontrado')
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`)
+})
